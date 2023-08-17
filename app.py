76819aa553bf557d0e711fee284833f315e855d9
Ahmed A. El-Otify <yukkiicode@gmail.com>
2023-08-17 21:34:24 +0300

    secure auth (hashed)

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
 from flask import Flask, render_template, request, jsonify
+from utils.backend import *
+import json, os
+# from flask_sqlalchemy import SQLAlchemy
 
+# from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+
+with open(os.path.abspath('config.json'), 'r') as file:
+    connection_params = json.load(file)
 
 app = Flask(__name__)
+# db = SQLAlchemy(app)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+# app.config['SECRET_KEY'] = 'jobati_secretKey000'
+
+# class User(db.Model, UserMixin):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(20), nullable=False)
+#     password = db.COlumn(db.String(80), nullable=False)
+
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'GET':
-        return render_template('register.html')
+        return render_template('login.html')
     else:
-        return "Under Maintenance.."
+        username = request.form['username']
+        password = request.form['password']
+        response_data = {
+            'username': username,
+            'password': password,
+
+        }
+        return validate_login(response_data, connection_params)
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
@@ -18,17 +42,13 @@ def register():
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
-        
-        print(username, password, email)
-        
-        # Create a dictionary to hold the data and return it as JSON response
         response_data = {
             'username': username,
             'password': password,
             'email': email
         }
-        
-        return jsonify(response_data)
+        register_account(response_data, connection_params)
+        return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port="5000")
--- utils/backend.py
+++ utils/backend.py
@@ -1,27 +1,72 @@
 import mysql.connector
-import json
+import json, bcrypt, re, hashlib
 
-with open('config.json', 'r') as file:
-    data = json.load(file)
-
-def get_json_from_database(project_id):
+def register_account(account_data, connection_params):
     try:
-        connection = mysql.connector.connect(**data)
+        connection = mysql.connector.connect(**connection_params)
         cursor = connection.cursor()
 
-        query = "SELECT json_column FROM your_table_name WHERE projectID = %s"
-        cursor.execute(query, (project_id,))
+        # Validate email format
+        if not re.match(r'^[\w\.-]+@[\w\.-]+$', account_data['email']):
+            print("Invalid email format")
+            return "Invalid email format"
+
+        # Hash the password
+        raw_password = account_data['password']
+        hashed_password = hashlib.sha256(raw_password.encode('utf-8')).hexdigest()
+        
+        sql = "INSERT INTO accounts (username, password, email) VALUES (%s, %s, %s)"
+        values = (account_data['username'], hashed_password, account_data['email'])
+
+        cursor.execute(sql, values)
+        connection.commit()  # Commit the transaction
+        
+        print("Account registered successfully!")
+    except mysql.connector.Error as error:
+        return f'Error: {error}'
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+
+
+def validate_login(account_data, connection_params):
+    try:
+        connection = mysql.connector.connect(**connection_params)
+        cursor = connection.cursor()
+
+        username = account_data['username']
+        entered_password = account_data['password']
+
+        # Retrieve user's hashed password and salt from the database
+        sql = "SELECT password FROM accounts WHERE username = %s"
+        cursor.execute(sql, (username,))
         result = cursor.fetchone()
 
         if result:
-            json_value = result[0]
-            return json.loads(json_value)
+            hashed_password = result[0]
 
-        # Return None if no matching projectID is found in the database
-        return None
+            # Hash the entered password using the retrieved salt
+            hashed_entered_password = hashlib.sha256(entered_password.encode('utf-8')).hexdigest()
+            
+            # Compare hashed passwords for validation
+            if hashed_entered_password == hashed_password:
+                print("Login successful!")
+                message = "Login successful!"
+            else:
+                print("Invalid username or password")
+                message = "Invalid username or password"
+        else:
+            print("Invalid username or password")
+            message = "Invalid username or password"
+        return message
 
     except mysql.connector.Error as error:
         print('Error:', error)
+        message = f'Error: {error}'
+        return message
 
     finally:
         if connection.is_connected():
